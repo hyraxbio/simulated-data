64c8c80982d9305436b7de4093622b09f36af5cb
Imogen Wright <imogen@hyraxbio.co.za>
2016-05-31 12:07:08 +0200

    bumping version number

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     description='Simulated data generator for HIV drug resistance testing',
     author='Imogen Wright',
     author_email='imogen@hyraxbio.co.za',
-    version='1.1.0',
+    version='1.2.0',
     packages=['seq2simulate'],
     scripts=['bin/simulate', 'bin/package-simulation', 'bin/sierra'],
     package_data={'seq2simulate': [
