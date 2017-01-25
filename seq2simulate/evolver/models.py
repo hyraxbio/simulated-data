209df2f085250765d1ff7555fdcd0697f2c8429a
Johann Eicher <johanneicher@gmail.com>
2017-01-25 14:50:26 +0200

    added Codon

--- seq2simulate/evolver/models.py
+++ seq2simulate/evolver/models.py
@@ -1,8 +1,19 @@
 
+class ValidationMixin(object):
+
+    def _is_valid_dna(self, seq):
+        valids = 'ATGCatgc-'
+        for i in set(seq):
+            if i not in valids: 
+                return False
+        return True, ''
 
 class CodonTable(object):
     """
     A simple implementation of the standard genetic code table.
+
+    Args:
+        stop_codons: include stop codons (default True)
     """
     def __init__(self, stop_codons=True):
         bases = ['t', 'c', 'a', 'g']
@@ -18,5 +29,37 @@ class CodonTable(object):
     def __repr__(self):
         return '<CodonTable {}>'.format(''.join(sorted([aa for aa in self.codon_dict.values()])))
 
+class Codon(ValidationMixin, object):
+    """
+    Codon object containing triplet nucleotides (and possibly indels).
+    
+    Args:
+        seq: the nucleotide sequence of the codon (default '---')
+    """
+    def __init__(self, seq='---'): 
+        self.seq = seq
+
+    @property
+    def seq(self):
+        return self._seq
+
+    @seq.setter
+    def seq(self, seq):
+        if not isinstance(seq, str):
+            raise ValueError('seq must be a str')
+        #if len(seq) > 3:
+        #    seq = seq[:3]    
+        while len(seq) < 3:
+            seq += '-'
+        if not self._is_valid_dna(seq):
+            raise ValueError('sequence is not valid')
+        self._seq = seq
+            
+    def __repr__(self):
+        return '<Codon {}>'.format(self._seq)
+        
+     
+
 if __name__=='__main__':
     ct = CodonTable(stop_codons=False)
+    c = Codon('atg')
--- seq2simulate/evolver/tests/test_models.py
+++ seq2simulate/evolver/tests/test_models.py
@@ -8,12 +8,26 @@ class ModelTester(unittest.TestCase):
         self.assertEqual(ct.__repr__(), '<CodonTable ***AAAACCDDEEFFGGGGHHIIIKKLLLLLLMNNPPPPQQRRRRRRSSSSSSTTTTVVVVWYY>')
 
     def test_codontable_can_exclude_stop_codons(self):
+        ct = models.CodonTable()
         cts = models.CodonTable(stop_codons=False)
         for codon in ['taa', 'tag', 'tga']:
+            self.assertIn(codon, ct.codon_dict)
+            getattr(ct, codon)
             self.assertNotIn(codon, cts.codon_dict)
             with self.assertRaises(AttributeError):
                 getattr(cts, codon)
             
+    def test_codon_init(self):
+        c = models.Codon()
+        self.assertEqual(c.__repr__(), '<Codon --->')
+
+    def test_codon_seq_validation(self):
+        c = models.Codon()
+        for seq in [1, 'abc']:
+            with self.assertRaises(ValueError):
+                c.seq = seq
+        c.seq = 'a'
+        self.assertEqual(c.seq, 'a--')
 
 if __name__=='__main__':
     unittest.main()
