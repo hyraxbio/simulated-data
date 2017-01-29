5c734c0018421d7bda1a54f1107aa8737ff3eb83
Imogen Wright <imogen@hyraxbio.co.za>
2017-01-29 10:58:18 +0200

    adding pcr error for randomized runs

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     description='Simulated data generator for HIV drug resistance testing',
     author='Imogen Wright',
     author_email='imogen@hyraxbio.co.za',
-    version='1.7.0',
+    version='1.8.0',
     packages=['seq2simulate'],
     scripts=[
         'bin/simulate', 
