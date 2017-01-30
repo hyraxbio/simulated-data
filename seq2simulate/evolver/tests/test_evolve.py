c45dc9ff6bb12c1d4eed3d342eaca1d2c6d08d8c
jeicher <johanneicher@gmail.com>
2017-01-30 13:28:22 +0200

    updated test with indels

--- seq2simulate/evolver/tests/test_evolve.py
+++ seq2simulate/evolver/tests/test_evolve.py
@@ -29,11 +29,17 @@ class EvolveTester(unittest.TestCase):
         for i in new_sequences:
             self.assertNotEqual(self.old_sequence, i) # this has a very low probability of failing
 
-    def test_evolve_high_insertion(self):
-        new_sequences = evolve.evolve(self.old_sequence, taxa=10, t=0.1, omega=1.1, kappa=1.5, lmbda=0.01, ti_td=10)
+    def test_evolve_no_insertion(self):
+        new_sequences = evolve.evolve(self.old_sequence, taxa=10, t=0.1, omega=1.1, kappa=1.5, lmbda=0.01, ti_td=0.0)
         self.assertEqual(len(new_sequences), 10)
         for i in new_sequences:
-            self.assertNotEqual(self.old_sequence, i) # this has a very low probability of failing
+            self.assertEqual(len(self.old_sequence), len(i))
+
+    def test_evolve_with_high_insertion(self):
+        new_sequences = evolve.evolve(self.old_sequence, taxa=10, t=0.1, omega=1.1, kappa=1.5, lmbda=0.1, ti_td=10.0)
+        self.assertEqual(len(new_sequences), 10)
+        len_new_sequences = sum([len(i) for i in new_sequences])
+        self.assertTrue(len_new_sequences != 10*len(self.old_sequence))
 
 if __name__=='__main__':
     unittest.main()
