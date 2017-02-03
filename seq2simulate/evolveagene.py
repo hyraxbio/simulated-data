e35b651fe15abc9706299059ef8c3a48a6072ac8
Johann Eicher <johanneicher@gmail.com>
2017-02-03 12:54:22 +0200

    working for roche data

--- seq2simulate/evolveagene.py
+++ seq2simulate/evolveagene.py
@@ -7,36 +7,20 @@ from subprocess import call
 from revolver.evolve import evolve
 
 fasta_output_filename = 'evolveagene_raw'
-evolveagene_extension = '_Unaligned.FASTA'
-
-CONSTANT, PURIFYING, POSITIVE, BOTH = range(4)
-SYMMETRIC, RANDOM = range(2)
-
-tree_types = {
-    SYMMETRIC: 'sym',
-    RANDOM: 'ran'
-}
-
-selection_types = {
-    CONSTANT: 'con',
-    PURIFYING: 'pur',
-    POSITIVE: 'pos',
-    BOTH: 'both'
-}
 
 def run(
-    sequence, tree_type, selection_type, 
-    num_taxa, branch_length, dnds, working_dir):
+    sequence, num_taxa, branch_length, dnds, lmbda, ti_td, working_dir):
     """
     Run the EvolveAGene4 sequence simulator.
 
     Args:
         sequence: A BioPython sequence to simulate.
-        tree_type: Either SYMMETRIC or RANDOM
-        selection_type: CONSTANT, PURIFYING, POSITIVE or BOTH.
         num_taxa: The number of sequences to simulate.
-        branch_length: The average substitution rate of the branches.
-        dnds: The chance that a nonsynonymous mutation will be accepted.
+        branch_length: The average substitution rate of the branches (by
+        default this is number of nucleotide substitutions per codon)
+        dnds: Ratio of non-synonymous to synonymous mutations
+        lmbda: probability of indel at locus/codon
+        ti_td: ratio of insertions to deletions
         working_dir: The director to which to save the generated file.
 
     Returns:
@@ -50,7 +34,8 @@ def run(
         taxa=num_taxa, 
         t=branch_length,
         omega=dnds, 
-        lmbda=0.001,
+        lmbda=lmbda,
+        ti_td=ti_td,
         codon_freq='F1x4',
         log=False,
         scale_q=True,
@@ -61,27 +46,10 @@ def run(
     sequence_file = os.path.join(working_dir, 
         fasta_output_filename + str(uuid.uuid4()))
 
-    ## EvolveAGene doesn't like fasta headers, so we write plain text
-    ## sequence rather than using SeqIO
-    #with open(sequence_file, 'w') as out:
-    #    out.write(str(sequence.seq))
-
-    #arg_list = ['EvolveAGene4',
-    #    '-f', '\"%s\"' % sequence_file, 
-    #    '-t', tree_types[tree_type],
-    #    '-n', str(num_taxa),
-    #    '-a', str(dnds),
-    #    '-b', str(branch_length),
-    #    '-ss', selection_types[selection_type],
-    #    ]
-
-    #call(" ".join(arg_list), shell=True)
-
     print 'evolved.'
     with open(sequence_file, 'w') as out:
-        #out.write(str(sequence.seq))
         for i,j in enumerate(new_sequences):
             out.write('>{}\n'.format(i))
             out.write(j+'\n')
     
-    return sequence_file# + evolveagene_extension
+    return sequence_file
