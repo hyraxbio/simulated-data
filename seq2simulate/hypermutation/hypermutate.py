443ece2376ef7d942ff426fc4cf7dcfe961da8e4
jeicher <johanneicher@gmail.com>
2017-02-07 16:32:42 +0200

    added mutation_probabilities and Sequence class

--- seq2simulate/hypermutation/hypermutate.py
+++ seq2simulate/hypermutation/hypermutate.py
@@ -1,19 +1,49 @@
-import numpy
 import re
+import mutation_probabilities
 
 
-def get_motif_indices(sequence, motif):
-    """
-    Find indices of substring motif in sequence.
+class Sequence(object):
+    def __init__(self, sequence):
+        """
+        Convenience class to deal with DNA sequences.
+        """
+        if not all(nucleotide in 'ATGC' for nucleotide in set(sequence)):
+            raise ValueError('Sequences may only contain A, T, G, and/or C.')
 
-    Args:
-        sequence: a string
-        motif: a substring
+        self.sequence = sequence
 
-    Returns: a list of left indices
-    """
-    indices = [m.start() for m in re.finditer(re.escape(motif), sequence)]
-    return indices
+    def get_motif_indices(self, motif):
+        """
+        Find indices of substring motif in sequence.
+    
+        Args:
+            motif: a substring
+    
+        Returns: a list of left indices
+        """
+        indices = [m.start() for m in re.finditer(re.escape(motif), self.sequence)]
+        return indices
+
+    def index_all_motifs(self, motif_probabilities=None):
+        """
+        Returns a dictionary of all motif indices in the sequence.
+
+        Args:
+            motif_probabilities: instance of mutation_probabilities
+        """
+        if motif_probabilities is None:
+            motif_probabilities = mutation_probabilities.KijakProbabilities()
+
+        motif_dict = {}
+        for motif in motif_probabilities.motifs:
+            motif_dict[motif] = self.get_motif_indices(motif) 
+
+        return motif_dict
+
 
 if __name__ == '__main__':
+    with open('../../data/split/Seq6_Sus', 'r') as f: 
+        seq = f.read()
+    seq = seq.replace('\n', '')[9:]
+    km = mutation_probabilities.KijakProbabilities()     
     pass
