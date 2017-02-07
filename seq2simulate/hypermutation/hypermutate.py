9a56761a50a12f970ac1703ce37143c89d03e707
jeicher <johanneicher@gmail.com>
2017-02-07 17:53:07 +0200

    added some useful properties to Sequence

--- seq2simulate/hypermutation/hypermutate.py
+++ seq2simulate/hypermutation/hypermutate.py
@@ -3,14 +3,22 @@ import mutation_probabilities
 
 
 class Sequence(object):
-    def __init__(self, sequence):
+
+    def __init__(self, sequence, motif_probabilities=None):
         """
         Convenience class to deal with DNA sequences.
+
+        Args:
+            sequence: a DNA string
+            motif_probabilities: instance of mutation_probabilities
         """
         if not all(nucleotide in 'ATGC' for nucleotide in set(sequence)):
             raise ValueError('Sequences may only contain A, T, G, and/or C.')
 
         self.sequence = sequence
+        self.sequence_motif_dict = None
+        if motif_probabilities is None:
+            self.motif_probabilities = mutation_probabilities.KijakProbabilities()
 
     def get_motif_indices(self, motif):
         """
@@ -24,22 +32,61 @@ class Sequence(object):
         indices = [m.start() for m in re.finditer(re.escape(motif), self.sequence)]
         return indices
 
-    def index_all_motifs(self, motif_probabilities=None):
+    def index_all_motifs(self):
         """
         Returns a dictionary of all motif indices in the sequence.
-
-        Args:
-            motif_probabilities: instance of mutation_probabilities
         """
-        if motif_probabilities is None:
-            motif_probabilities = mutation_probabilities.KijakProbabilities()
 
         motif_dict = {}
-        for motif in motif_probabilities.motifs:
-            motif_dict[motif] = self.get_motif_indices(motif) 
+        for motif in self.motif_probabilities.motifs:
+            motif_indices = self.get_motif_indices(motif) 
+            if len(motif_indices) > 0:
+                motif_dict[motif] = motif_indices
 
-        return motif_dict
+        self.sequence_motif_dict = motif_dict
 
+    def mutate_sequence(self, n):
+        """
+        Perform n mutations of self.sequence.
+
+        Args:
+            n: int
+        """
+        if self.sequence_motif_dict is None:
+            print('First indexing sequence.')
+            self.index_all_motifs()
+
+        mutations = []
+        #while len(mutations) < n:
+            
+   
+    @property 
+    def num_motifs(self):
+        try:
+            num_motifs = sum([len(j) for i, j in self.sequence_motif_dict.iteritems()])
+        except AttributeError:
+            num_motifs = 0
+        return num_motifs
+
+    @property
+    def motif_probs(self):
+        """
+        A shortened rescaled dictionary of motif mutation probabilities
+        including only the present motifs.
+
+        """
+        try:
+            remaining_probabilities_sum = sum([self.motif_probabilities.motifs[i] for i in self.sequence_motif_dict])
+            remaining_probabilities = {i: self.motif_probabilities.motifs[i] / remaining_probabilities_sum for i in self.sequence_motif_dict}
+        except AttributeError:
+            remaining_probabilities = {}
+        return remaining_probabilities
+            
 
 if __name__ == '__main__':
+    seq = 'atgcaacggcgattatacgtatcgtgcatcgatcatcgcatgcaacggcgattatacgtatcgtgcatcgatcatggtcgc'.upper()
+    seq = Sequence(seq)
+    #km = mutation_probabilities.KijakProbabilities()     
+    i = seq.index_all_motifs()
+
     pass
--- seq2simulate/hypermutation/tests/test_hypermutation.py
+++ seq2simulate/hypermutation/tests/test_hypermutation.py
@@ -7,7 +7,7 @@ class HypermutTester(unittest.TestCase):
 
 
     def setUp(self):
-        self.old_sequence = 'atgcaacggcgattatacgtatcgtgcatcgatcatcgcatgcaacggcgattatacgtatcgtgcatcgatcatcgc'.upper()
+        self.old_sequence = 'atgcaacggcgattatacgtatcgtgcatcgatcatcgcatgcaacggcgattatacgtatcgtgcatcgatcatggtcgc'.upper()
         self.seq = hypermutate.Sequence(self.old_sequence)
 
 
@@ -20,12 +20,12 @@ class HypermutTester(unittest.TestCase):
 
     def test_get_motif_indices(self):
         indices = self.seq.get_motif_indices('ATG')
-        self.assertEqual(indices, [0, 39])
+        self.assertEqual(indices, [0, 39, 73])
 
 
     def test_index_all_motifs(self):
-        indices = self.seq.index_all_motifs()
-        self.assertEqual(indices['CGGC'], [6, 45])
+        self.seq.index_all_motifs()
+        self.assertEqual(self.seq.sequence_motif_dict['CGGC'], [6, 45])
 
 
     def test_motif_probabilities(self):
@@ -36,3 +36,15 @@ class HypermutTester(unittest.TestCase):
     def test_kijak_probabilities(self):
         motif_probabilities = mutation_probabilities.KijakProbabilities() 
         self.assertTrue(numpy.isclose(sum(motif_probabilities.motifs.values()), 1))
+
+
+    def test_num_motifs(self):
+        self.seq.index_all_motifs()
+        self.assertEqual(self.seq.num_motifs, 7)
+
+
+    def test_motif_probs(self):
+        self.seq.index_all_motifs()
+        expected_answer = {'CGGC': 0.0, 'TGGT': 0.3890518084067808, 'GA': 0.6109481915932192}
+        for i, j in expected_answer.iteritems():
+            self.assertTrue(numpy.isclose(self.seq.motif_probs[i], j))
