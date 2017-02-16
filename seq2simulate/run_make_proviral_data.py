8d3105338d4a409223f88431eef11291f0f1ac10
Johann Eicher <johanneicher@gmail.com>
2017-02-16 14:11:38 +0200

    handle paired-end data correctly in run_make_proviral_data.py

--- seq2simulate/run_make_proviral_data.py
+++ seq2simulate/run_make_proviral_data.py
@@ -79,35 +79,76 @@ def run_proviral(sequences, working_dir, out_dir, platform, paired_end, proviral
 
     platf = getattr(plat, platform)
 
+    f = art.simulate(sequences, platf, platf.coverage, paired_end, out_dir)
     fastq_file0, sam_file0 = art.simulate(sequences, platf, platf.coverage, paired_end, out_dir)
     fastq_file1, sam_file1 = art.simulate(hypermutated_sequence_file, platf, platf.coverage, paired_end, out_dir)
 
-    with open(fastq_file0, 'r') as f:
-        fq0 = [i for i in parse_fastq(f)]
-    with open(fastq_file1, 'r') as f:
-        fq1 = [i for i in parse_fastq(f)]
+    if paired_end:
+        fastq_file0a, fastq_file0b = fastq_file0
+        fastq_file1a, fastq_file1b = fastq_file1
+        open_files = []
+        for fq in [fastq_file0a, fastq_file0b, fastq_file1a, fastq_file1b]:
+            with open(fq, 'r') as f:
+                open_files.append([i for i in parse_fastq(f)])
+        fq0a, fq0b, fq1a, fq1b = open_files
+    else:
+        open_files = []
+        for fq in [fastq_file0, fastq_file1]:
+            with open(fq, 'r') as f:
+                open_files.append([i for i in parse_fastq(f)])
+        fq0, fq1 = open_files
 
-    n_reads = min(len(fq0), len(fq1))
+    n_reads = min([len(i) for i in open_files])
     n_proviral_reads = int(proviral_fraction * n_reads)
-
     print('Sampling {0} proviral reads out of {1} total reads (r={2:.3f}).'.format(n_proviral_reads, n_reads, n_proviral_reads/float(n_reads)))
 
-    mixed_fastq = []
-    while len(mixed_fastq) < n_proviral_reads:
-        i = random.randint(0, len(fq1) - 1)
-        mixed_fastq.append(fq1.pop(i))
-    while len(mixed_fastq) < n_reads:
-        i = random.randint(0, len(fq0) - 1)
-        mixed_fastq.append(fq0.pop(i))
-   
-    mixed_fastq = ''.join([j for i in mixed_fastq for j in i])
+    if paired_end:
+    
+        mixed_fastq_f, mixed_fastq_r = [], []
+        while len(mixed_fastq_f) < n_proviral_reads:
+            i_f = random.randint(0, len(fq1a) - 1)
+            i_r = random.randint(0, len(fq1b) - 1)
+            mixed_fastq_f.append(fq1a.pop(i_f))
+            mixed_fastq_r.append(fq1b.pop(i_r))
+        while len(mixed_fastq_f) < n_reads:
+            i_f = random.randint(0, len(fq0a) - 1)
+            i_r = random.randint(0, len(fq0b) - 1)
+            mixed_fastq_f.append(fq0a.pop(i_f))
+            mixed_fastq_r.append(fq0b.pop(i_r))
+       
+        mixed_fastq_f = ''.join([j for i in mixed_fastq_f for j in i])
+        mixed_fastq_r = ''.join([j for i in mixed_fastq_r for j in i])
+    
+        full_filename_f = os.path.join(
+            out_dir, 
+            "mixed_hyperdata1.fastq",
+        )
+        full_filename_r = os.path.join(
+            out_dir, 
+            "mixed_hyperdata2.fastq",
+        )
+        with open(full_filename_f, 'w') as f:
+            f.write(mixed_fastq_f) 
+        with open(full_filename_r, 'w') as f:
+            f.write(mixed_fastq_r) 
 
-    full_filename = os.path.join(
-        out_dir, 
-        "mixed_hyperdata.fastq",
-    )
-    with open(full_filename, 'w') as f:
-        f.write(mixed_fastq) 
+    else:
+        mixed_fastq = []
+        while len(mixed_fastq) < n_proviral_reads:
+            i = random.randint(0, len(fq1) - 1)
+            mixed_fastq.append(fq1.pop(i))
+        while len(mixed_fastq) < n_reads:
+            i = random.randint(0, len(fq0) - 1)
+            mixed_fastq.append(fq0.pop(i))
+   
+        mixed_fastq = ''.join([j for i in mixed_fastq for j in i])
+
+        full_filename = os.path.join(
+            out_dir, 
+            "mixed_hyperdata.fastq",
+        )
+        with open(full_filename, 'w') as f:
+            f.write(mixed_fastq) 
 
     print 'Output saved in:', out_dir
     return True
