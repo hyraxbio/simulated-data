1ab04171c478c1f67965d35caa3f7c8b01ca0f26
Johann Eicher <johanneicher@gmail.com>
2017-02-23 13:49:25 +0200

    removed some test files

--- _test0.sh
+++
@@ -1,2 +0,0 @@
-pip uninstall simulate && pip install .
-make-proviral-data --out /home/jeicher/Apps/simulated-data/_tests/output --sequences data/split/Seq7_Sus --platform roche --hypermutation-rate 3 --working-dir ./_tests/output/working
