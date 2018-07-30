d487eb898a9e130ffdc57847039240bc826a83d3
Imogen Wright <imogen@hyraxbio.co.za>
2018-07-30 16:14:06 +0200

    updating package management and fixing a bug

--- seq2simulate/sequence.py
+++ seq2simulate/sequence.py
@@ -10,7 +10,7 @@ class Sequence:
         self.years_infected = 1
         self.susceptible = susceptible
         self.resistant = resistant
-        self.drms = sierra.get_drms(resistant, known_drms=hiv_drms)
+        self.drms = sierra.get_drms(resistant, known_drms=hiv_drms.drms)
         self.pcr_error = False
         self.human_error = False
         self.env_error = False
